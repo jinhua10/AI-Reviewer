c3d060458aa0def4996e497d988908ec12c0e847
Jinhua Yu <1015770492@qq.com>
2025-11-13 19:09:09 +0800

    git 统一，移除非必要的适配器

--- src/test/java/top/yumbo/ai/reviewer/ASTAnalysisExample.java
+++ src/test/java/top/yumbo/ai/reviewer/ASTAnalysisExample.java
@@ -10,7 +10,6 @@ import top.yumbo.ai.reviewer.domain.model.ast.CodeInsight;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 /**
