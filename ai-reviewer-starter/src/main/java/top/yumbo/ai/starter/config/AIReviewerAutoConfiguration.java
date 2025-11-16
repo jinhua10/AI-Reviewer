7d26aeac5b0b1a26b0b15527481e6697fccf592c
Jinhua Yu <1015770492@qq.com>
2025-11-16 23:13:27 +0800

    版本管理

--- ai-reviewer-starter/src/main/java/top/yumbo/ai/starter/config/AIReviewerAutoConfiguration.java
+++ ai-reviewer-starter/src/main/java/top/yumbo/ai/starter/config/AIReviewerAutoConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import top.yumbo.ai.adaptor.ai.HttpBasedAIAdapter;
 import top.yumbo.ai.adaptor.parser.JavaFileParser;
 import top.yumbo.ai.adaptor.parser.JavaScriptFileParser;
@@ -27,6 +28,7 @@ public class AIReviewerAutoConfiguration {
     AIReviewerProperties properties;
 
     @Bean
+    @Primary
     public AdapterRegistry adapterRegistry() {
         // 直接使用从配置文件绑定的 AIConfig
         AIConfig aiConfig = properties.getAi();
