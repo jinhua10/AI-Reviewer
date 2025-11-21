d06150a30ea5bdc6d0ebb9b9676a26c23a9943b9
Jinhua Yu <1015770492@qq.com>
2025-11-21 14:18:39 +0800

    s3

--- application-demo/AWS-S3-Download-Util/src/main/java/top/yumbo/ai/s3/AWSS3Application.java
+++ application-demo/AWS-S3-Download-Util/src/main/java/top/yumbo/ai/s3/AWSS3Application.java
@@ -1,6 +1,22 @@
 package top.yumbo.ai.s3;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+@EnableAutoConfiguration
 public class AWSS3Application {
 
+    public static void main(String[] args) {
+        log.info("Starting AWS S3 Application with args: {}", String.join(", ", args));
+        log.info("Classpath: {}", System.getProperty("java.class.path"));
+
+        SpringApplication app = new SpringApplication(AWSS3Application.class);
+        app.setAdditionalProfiles(); // 使用默认 profile
+        app.run(args);
+    }
 }
